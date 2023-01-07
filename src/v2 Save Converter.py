feaaf1e116e897b53c4d91ce2282fc0f7f745122
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2023-01-07 18:59:02 +1100

    Minor adjustments to Save Converter

--- src/v2 Save Converter.py
+++ src/v2 Save Converter.py
@@ -1,11 +1,11 @@
 import toml
 import json
 from os.path import exists
-from Scripts import utils
+from Scripts.utils import force_exit, log
 
 print("""This is used to convert v1 saves to v2, as it uses a completely different format.
 Instructions:
-1. Place this file in the same folder as your 'save.json' and 'utils.py' file.
+1. Place this file in the same folder as your 'save.json'.
 2. Type 'READY' in all caps to start. Press Enter to exit.""")
 
 while True:
@@ -14,14 +14,17 @@ while True:
         if choice == "READY":
 
             if not exists("save.json"):
-                utils.force_exit("Save file could not be found. Please ensure you have read the instructions.")
+                force_exit("Save file could not be found.")
+                
+            if exists("save.toml"):
+                force_exit("v2 save file already exists.")
 
             print("Loading JSON...")
             with open("save.json", "r") as f:
                 old_save = json.loads(f.read())
 
             if not old_save.get("checksum", False):
-                utils.force_exit("JSON file is invalid or missing checksum.")
+                force_exit("JSON file is invalid or missing checksum.")
 
             print("Verifying checksum...")
             sum_of_numbers = 0  # Sum thing
@@ -34,74 +37,71 @@ while True:
 
             # Checks for length
             if not (old_save["checksum"][0] == len(str(sum_of_numbers))):
-                utils.force_exit(f"Invalid length, "
+                force_exit(f"Invalid length, "
                                  f"got '{old_save['checksum'][0]}'.")
 
             # Checks for valid symbol
             if not (old_save["checksum"][1] == "/" or old_save["checksum"][1] == "%"):
-                utils.force_exit(f"Invalid symbol, "
+                force_exit(f"Invalid symbol, "
                                  f"got '{old_save['checksum'][1]}'.")
 
             # Finally, checks if the maths works out
             if old_save["checksum"][1] == "/":
                 if not (sum_of_numbers / 2 == old_save["checksum"][2]):
-                    utils.force_exit(f"Invalid division, "
+                    force_exit(f"Invalid division, "
                                      f"got '{old_save['checksum'][2]}'.")
             elif old_save["checksum"][1] == "%":
                 if not (str(sum_of_numbers / 2).split('.')[1] == str(old_save["checksum"][2])):
-                    utils.force_exit(f"Invalid decimal, "
+                    force_exit(f"Invalid decimal, "
                                      f"got '{old_save['checksum'][2]}'.")
             else:
-                utils.force_exit(f"Invalid symbol further into execution, got '{old_save['checksum'][1]}'.")
+                force_exit(f"Invalid symbol further into execution, got '{old_save['checksum'][1]}'.")
 
             print("Creating new TOML file...")
-            if exists("save.toml"):
-                utils.force_exit("'save.toml' already exists.")
-            else:
-                with open("save.toml", "x"):
-                    pass
+            with open("save.toml", "x"):
+                pass
 
-                i = 0
-                # I spent two hours trying to figure out why the TOML file didn't work
-                # And it fucking turns out
-                # It's a problem with TOML itself, because arrays are homogeneous
-                # Fuck me
-                for x in old_save["checksum"]:
-                    old_save["checksum"][i] = str(old_save["checksum"][i])
-                    i += 1
+            i = 0
+            # I spent two hours trying to figure out why the TOML file didn't work
+            # And it fucking turns out
+            # It's a problem with TOML itself, because arrays are homogeneous
+            # Fuck me
+            for x in old_save["checksum"]:
+                old_save["checksum"][i] = str(old_save["checksum"][i])
+                i += 1
 
-                new_save = {
-                    "coins": old_save.get("coins", 0),
-                    "debt": old_save.get("debt", 0),
-                    "shinies": 0,
+            new_save = {
+                "coins": old_save.get("coins", 0),
+                "debt": old_save.get("debt", 0),
+                "shinies": 0,
 
-                    "items": {
-                        "auto_typer": old_save.get("autoTyper", 0),
-                        "backroom_deals": old_save.get("backroomDeals", 0),
-                        "weighted_chip": old_save.get("weightedChip", 0),
+                "items": {
+                    "auto_typer": old_save.get("autoTyper", 0),
+                    "backroom_deals": old_save.get("backroomDeals", 0),
+                    "weighted_chip": old_save.get("weightedChip", 0),
+                },
+
+                "core": {
+                    "wins": 0,
+                    "runs": 0,
+                    "checksum": [],
+                },
+
+                "preferences": {
+                    "patch_notes": True
+                },
+
+                "archived": {
+                    "v1": {
+                        # This probably doesn't need to be kept, but better safe than sorry
+                        "checksum": old_save.get("checksum", []),
+                        "runs": old_save.get("runs", 0),
                     },
+                },
+            }
 
-                    "core": {
-                        "wins": 0,
-                        "runs": 0,
-                        "checksum": [],
-                    },
-
-                    "preferences": {
-                        "patch_notes": True
-                    },
-
-                    "archived": {
-                        "v1": {
-                            # This probably doesn't need to be kept, but better safe than sorry
-                            "checksum": old_save.get("checksum", []),
-                            "runs": old_save.get("runs", 0),
-                        },
-                    },
-                }
-
-                with open("save.toml", "w") as f:
-                    f.write(toml.dumps(new_save))
+            with open("save.toml", "w") as f:
+                f.write(toml.dumps(new_save))
 
             print("Done. Check your directory to see if a file called 'save.toml' exists.")
             print("'save.json' is now useless (unless you plan on playing v1), and you may delete it.")
@@ -112,5 +112,5 @@ while True:
             break
 
     except Exception as err:
-        print("An error has occurred. See logs for more info.")
-        utils.force_exit(f"{type(err).__name__}: {err}")
+        print("An error has occurred. See logs for more info.\n")
+        log(f"{type(err).__name__}: {err}")
